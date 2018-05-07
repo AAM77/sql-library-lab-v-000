e47718ad07c11b9ce35b67954d9a6de1ac54573d
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-07 18:54:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_series_title_with_most_human_characters
    INNER JOIN characters
    ON series.id = characters.series_id
    GROUP BY species
-   WHERE species = "human"
+   HAVING species = "human"
    ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
