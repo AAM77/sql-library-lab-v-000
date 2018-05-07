b2607f05ef53c27975fcfdaeacb3460b320e9aef
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-07 18:46:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,8 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.name FROM series
-   INNER JOIN "
+   INNER JOIN characters
+   ON series."
 end
 
 def select_character_names_and_number_of_books_they_are_in
