04886dcc427ee3fb5462e5ff9692555003bab30c
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-07 18:53:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
    INNER JOIN characters
    ON series.id = characters.series_id
-   "
+   GROUP BY species
+   HAVING MAX(COUNT(species));"
 end
 
 def select_character_names_and_number_of_books_they_are_in
