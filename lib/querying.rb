eca16381fc1e1195e1c32ddfc8bb03940f25cfed
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-07 18:53:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_series_title_with_most_human_characters
    INNER JOIN characters
    ON series.id = characters.series_id
    GROUP BY species
-   HAVING MAX(COUNT(species));"
+   ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
