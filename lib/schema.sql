ca3e3bc9199088b98cedc870d59324eb4e295a18
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-07 16:42:21 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,25 +1,27 @@
-CREATE TABLE characters (
+CREATE TABLE series (
   id INTEGER PRIMARY KEY,
-   
-)
+  title TEXT,
+  author_id INTEGER,
+  sub_genre_id INTEGER
+);
+
+###
+
+CREATE TABLE sub-genre (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+###
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
 
 ###
 
 CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
    
-)
-
-###
-
-CREATE TABLE characters (
-  id INTEGER PRIMARY KEY,
-   
-)
-
-###
-
-CREATE TABLE characters (
-  id INTEGER PRIMARY KEY,
-   
-)
+);
