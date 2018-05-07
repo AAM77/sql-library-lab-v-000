aae2375c9add62385537f5f49d96e8d776d9fe4c
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-07 19:05:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,10 +25,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, species, COUNT(species) FROM series
+  "SELECT series.title FROM series
    INNER JOIN characters
    ON series.id = characters.series_id
-   GROUP BY species;"
+   GROUP BY series.title, species
+   HAVING species = 'human'
+   ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
