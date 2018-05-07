60bf62f5449f9afcb17e4ce0455fd10187defe32
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-07 19:33:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,5 +36,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id) FROM characters
    INNER JOIN character_books
-   ON characters.id = character_books.id;"
+   ON characters.id = character_books.id
+   GROUP BY characters.name;"
 end
